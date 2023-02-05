3b996fcc9a7663003edb571669a98e88569fa9de
Nguyễn Hà Tuấn Nguyên <118656767+AsahiOw@users.noreply.github.com>
2023-02-05 22:01:07 +0700

    add more feature in setup part

--- detail.js
+++ detail.js
@@ -10,3 +10,21 @@ function stickyEffect()
     header.classList.remove("sticky");
   }
 }
+
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
