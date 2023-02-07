8f9b162e4afd19f6bd3b0c81fd522aaaf1a74800
Nguyễn Hà Tuấn Nguyên <118656767+AsahiOw@users.noreply.github.com>
2023-02-07 18:37:46 +0700

    update transition

--- detail.js
+++ detail.js
@@ -1,30 +1,55 @@
-// sticky effect for header
-window.onscroll = function() {stickyEffect()};
-var header = document.getElementById("stickyHeader");
-var sticky = header.offsetTop;
-function stickyEffect() 
-{
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
-
-
+//swipe photo function
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   loop: true,
 
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+      el: '.swiper-pagination',
   },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
   },
 
-});
+  });
+
+
+//left and right function for Blue Archive card
+let activeIndex = 0;
+
+const groups = document.getElementsByClassName("card-group");
+
+const RightClick = () => {
+  const nextIndex = activeIndex + 1 <= groups.length - 1 ? activeIndex + 1 : 0;
+  
+  const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
+        nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+        
+  currentGroup.dataset.status = "after";
+  
+  nextGroup.dataset.status = "becoming-active-from-before";
+  
+  setTimeout(() => {
+    nextGroup.dataset.status = "active";
+    activeIndex = nextIndex;
+  });
+}
+
+const LeftClick = () => {
+  const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : groups.length - 1;
+  
+  const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
+        nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+  
+  currentGroup.dataset.status = "before";
+  
+  nextGroup.dataset.status = "becoming-active-from-after";
+  
+  setTimeout(() => {
+    nextGroup.dataset.status = "active";
+    activeIndex = nextIndex;
+  });
+}
